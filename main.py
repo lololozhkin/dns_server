44dc80f953be163efc1741c5e06c6e7e44602282
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-10-25 22:27:52 +0500

    multithreading realization instead of stupido async

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import asyncio
 import socket
+import threading
 from DnsPacketParser import DnsPacket
 from RecordClass import RecordClass
 from RecordType import RecordType
@@ -13,28 +13,21 @@ from utils import get_str_ip_from_bytes, get_ip_in_bytes_from_string
 ROOT_DNS = '198.41.0.4'
 DNS_PORT = 53
 
-stupido_query = b"\xc2\x7d\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x09\x68" \
-                b"\x61\x62" \
-                b"\x72\x61\x68\x61\x62\x72\x02\x72\x75\x00\x00\x01\x00\x01" \
-                b"\x00\x00" \
-                b"\x29\x02\x00\x00\x00\x00\x00\x00\x00"
 
-
-async def make_request(req_ip, req_domain, sock):
+def make_request(req_ip, req_domain, sock):
     query = Query(req_domain.encode())
     to_send = DnsPacketComposer([query],
                                 flags=generate_flags(),
                                 id=random.randint(0, 0xFFFF))
     sock.sendto(to_send.compose_packet(), (req_ip, DNS_PORT))
 
-    loop = asyncio.get_event_loop()
-    data, _ = await loop.run_in_executor(None, lambda: sock.recvfrom(10000))
+    data, _ = sock.recvfrom(10000)
 
     return DnsPacket(data)
 
 
-async def get_ip(req_ip, req_domain, sock):
-    response = await make_request(req_ip, req_domain, sock)
+def get_ip(req_ip, req_domain, sock):
+    response = make_request(req_ip, req_domain, sock)
     additional_with_ip = list(
         filter(lambda x: x.r_type == RecordType.A, response.additional))
 
@@ -43,18 +36,18 @@ async def get_ip(req_ip, req_domain, sock):
 
     if len(additional_with_ip):
         ip = get_str_ip_from_bytes(additional_with_ip[0].data)
-        return await get_ip(ip, req_domain, sock)
+        return get_ip(ip, req_domain, sock)
     else:
         ns_authorities = list(
             filter(lambda x: x.r_type == RecordType.NS, response.authorities))
 
         if len(ns_authorities):
             ns = ns_authorities[0].data.decode()
-            authority_ip = await get_ip(ROOT_DNS, ns, sock)
-            return await get_ip(authority_ip, req_domain, sock)
+            authority_ip = get_ip(ROOT_DNS, ns, sock)
+            return get_ip(authority_ip, req_domain, sock)
 
 
-async def serve_client(sock, query, addr):
+def serve_client(sock, query, addr):
     socket_for_dns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     packet = DnsPacket(query)
     req_id = packet.id
@@ -64,7 +57,7 @@ async def serve_client(sock, query, addr):
         return
     domain = packet.queries[0].name
 
-    ip = await get_ip(ROOT_DNS, domain.decode(), socket_for_dns)
+    ip = get_ip(ROOT_DNS, domain.decode(), socket_for_dns)
     response_packet = DnsPacketComposer(id=req_id)
     if ip is not None:
         response = ResourceRecord(domain, data=get_ip_in_bytes_from_string(ip))
@@ -74,17 +67,25 @@ async def serve_client(sock, query, addr):
     sock.sendto(response_packet.compose_packet(), addr)
 
 
-async def main():
+def main():
     listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     listen_sock.bind(('127.0.0.1', 13337))
 
+    threads = set()
     while True:
+        for th in list(threads):
+            if not th.is_alive():
+                th.join()
+                threads.remove(th)
+
         data, addr = listen_sock.recvfrom(1024)
 
-        print(f'msg: {data}')
-        task = asyncio.create_task(serve_client(listen_sock, data, addr))
-        await task
+        th = threading.Thread(
+            target=lambda: serve_client(listen_sock, data, addr))
+        threads.add(th)
+        th.daemon = True
+        th.start()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
